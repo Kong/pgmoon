b1ee028facfa298d0350bf811898a9dc77c607ac
Murillo Paula <murillo@murillopaula.com>
2021-01-14 11:14:04 -0300

    fix(init) make tests green and pass correct options to luasec_opts
    ngx.ssl is only available in openresty land, whereas this plugin's CI does not have it enabled/installed to run tests.
    
    Also fix a regression where luasec_opts had incorrect parameters.

--- pgmoon/init.lua
+++ pgmoon/init.lua
@@ -6,8 +6,12 @@ do
   local _obj_0 = require("pgmoon.bit")
   rshift, lshift, band, bxor = _obj_0.rshift, _obj_0.lshift, _obj_0.band, _obj_0.bxor
 end
-local pl_file = require("pl.file")
-local ngx_ssl = require("ngx.ssl")
+local pl_file
+local ngx_ssl
+if ngx then
+  pl_file = require("pl.file")
+  ngx_ssl = require("ngx.ssl")
+end
 local unpack = table.unpack or unpack
 local VERSION = "2.2.0"
 local _len
